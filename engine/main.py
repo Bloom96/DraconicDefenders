88372b9b6c5acefc7fec5bbe7b7dfd1bbf862973
ValentinSilman <silman.valentin@gmail.com>
2026-02-01 21:20:36 +0100

    Fixing HP progress bar error, and creating mana progress bar too

--- engine/main.py
+++ engine/main.py
@@ -7,16 +7,33 @@ class MainConsole(App):
     CSS_PATH = "console_layout.tcss"
 
     def compose(self) -> ComposeResult:
-        yield ProgressBar(total = 100, progress=100, show_eta=False, id="hp_bar")
+        #setting up the progress bar to it's default value
+        yield ProgressBar(total = 100, show_eta=False, id="hp_bar")
+        yield ProgressBar(total = 100, show_eta=False, id="mana_bar")
     
+    def on_mount(self):
+        hp_bar = self.query_one("#hp_bar", ProgressBar)
+        mana_bar = self.query_one("#mana_bar", ProgressBar)
+
+        hp_bar.progress = 100
+        mana_bar.progress = 100
+
+    def spell_cast(self, spell_cost):
+        mana_bar = self.query_one("#mana_bar", ProgressBar)
+        mana_bar -= spell_cost
+
+    # taking the damage is getting accounted for here
     def take_damage(self, amount):
         hp_bar = self.query_one("#hp_bar", ProgressBar)
         hp_bar.progress -= amount
 
+    # leveling up autoresets the HP
     def level_up(self):
         hp_bar = self.query_one("#hp_bar", ProgressBar)
-        hp_bar.progress = 100
+        mana_bar = self.query_one("#mana_bar", ProgressBar)
 
+        hp_bar.progress = 100
+        mana_bar.progress = 100
 
 app = MainConsole()
 if __name__ == "__main__":
